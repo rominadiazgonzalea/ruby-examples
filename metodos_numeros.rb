994be632b2fb540b2462aa87c5cfe7f5d994af86
rominadiazgonzalea <rominad2@hotmail.com>
2022-05-27 10:12:49 -0400

    Agregando archivo de metodo de numeo

---
+++ metodos_numeros.rb
@@ -0,0 +1,21 @@
+
+numero_entero=1776
+#metodo para numero entero siguiente o succesor
+puts numero_entero.next
+puts numero_siguiente=numero_entero.succ
+#metodo para validar si un numero es par o imparme
+puts numero_entero.odd?
+puts numero_entero.even?
+puts numero_entero.odd?
+puts numero_siguiente.even?
+#conversion de entero o flotante
+puts numero_siguiente/numero_entero.to_f
+puts 800/270.to_f
+#ponerle en digitos un numero de un numero
+print   356.digits
+print 78905.digits
+#redondear un numero flotante
+puts 5609.78203.round
+puts 5609.78203.to_i
+puts 5609.78203.floor
+puts 5609.78203.ceil
