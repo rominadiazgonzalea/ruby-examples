4e999b488c1e6df40be5bcc33bde5ac62ba22203
rominadiazgonzalea <rominad2@hotmail.com>
2022-05-27 10:11:41 -0400

    Agregando archivo

---
+++ variable.rb
@@ -0,0 +1,14 @@
+# sumando dos  variable
+x = 10
+y = 20
+suma = x + y
+puts suma
+# multiplicando dos variable
+precio_dolar = 20
+precio_mx= 770
+convension = precio_dolar * precio_mx
+puts convension
+#variable tipo texto
+texto_saludo= "hola desde mexico"
+puts texto_saludo
+puts "hasta el proximo codigo"
