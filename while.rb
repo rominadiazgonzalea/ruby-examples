513f63f67652ee90326fdc5c36d3e6838051235f
rominadiazgonzalea <rominad2@hotmail.com>
2022-06-07 12:00:37 -0400

    Agregando while.rb

---
+++ while.rb
@@ -0,0 +1,14 @@
+i= 5
+while i > 0
+    puts "conteo: #{i}"
+    i -=1
+end
+puts "Despegando"
+
+j = 0
+suma=0
+while j < 10
+    suma+=j
+    j+=1
+end
+puts "La suma de los primero 10 numero es #{suma}"
