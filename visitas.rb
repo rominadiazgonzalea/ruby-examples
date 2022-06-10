e5b4a76895fd6c3b389367a49c37dcfe83858429
rominadiazgonzalea <rominad2@hotmail.com>
2022-06-10 12:54:07 -0400

    Agregando visitas.rb

---
+++ visitas.rb
@@ -0,0 +1,17 @@
+visitas = [10000,800,250,300,500,2500]
+
+def promedio(arreglo)
+promedio = arreglo.sum/arreglo.count
+# sum=0
+# arreglo.each do |j|
+#  sum+=j
+# end
+# return sum/arreglo.count
+end
+
+puts promedio (visitas)
+
+
+
+
+
