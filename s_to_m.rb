7b2560f0d40b82c225d922f402f61efe4d40364e
rominadiazgonzalea <rominad2@hotmail.com>
2022-06-12 22:28:48 -0400

    Agregando s_to_m.rb

--- s_to_m.rb
+++ s_to_m.rb
@@ -1,10 +1,10 @@
 def segunoAminutos(arreglo)
     minutos=[]
-arreglo.each do |j|
-        # tminutos= j/60.00
-        # redondear= tminutos.round(2)
-        minutos.push {(j.to_f/60).round(2)}
-end
+    arreglo.each do |j|
+        # tMinutos= j/60.00
+        # redondear= tMinutos.round(2)
+        minutos.push((j.to_f/60).round(2))
+    end
 return minutos
 end
 procesos =[4003,500,893,3232,7733,233,30,5000]
