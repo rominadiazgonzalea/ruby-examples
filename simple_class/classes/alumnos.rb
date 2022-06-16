b8743eca1b70d260fddbc81c84f18e8c72aba60c
rominadiazgonzalea <rominad2@hotmail.com>
2022-06-15 23:23:04 -0400

    Agregando simple_class

---
+++ simple_class/classes/alumnos.rb
@@ -0,0 +1,8 @@
+class Alumnos   
+     
+    def full_name(name,last_name)
+        puts "El nombre completo es: #{name.capitalize}, #{last_name.capitalize}"   
+     end
+    
+    
+    end
---
+++ simple_class/init.rb
@@ -0,0 +1,9 @@
+require_relative "classes/alumnos"
+
+objecto = Alumnos.new
+
+
+objecto.full_name('Roberto',"Hernandez")
+
+
+
